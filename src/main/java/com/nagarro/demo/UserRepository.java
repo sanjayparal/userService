6648729266057b4df579818830aa66a5919209b2
sanjayparal <sanjay.paral@nagarro.com>
2020-07-06 14:59:07 +0530

    adding code to user service

---
+++ src/main/java/com/nagarro/demo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nagarro.demo;
+
+import com.nagarro.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into user (id,name,email,age) values(1,'Sanjay Paral','sanjay.paral@nagarro.com',25);
